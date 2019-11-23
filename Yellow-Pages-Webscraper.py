afe9f4a3f742e3b32a9a8d46436db2123391064e
Shamal Prasad <41844276+shammy99@users.noreply.github.com>
2019-11-24 09:47:53 +1100

    All Functions are operating but need to be combined
    All functions are operating. I have to now:
    - Combine all the functions
    - Create a data Structure to store information
    - Export Data as a csv and as a JSON File

--- Yellow-Pages-Webscraper.py
+++ Yellow-Pages-Webscraper.py
@@ -1,5 +1,4 @@
 import selenium
-from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 import sys
@@ -7,16 +6,18 @@ import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
 Postcode_Details = pd.read_csv("NSW-Postcodes.csv")
 
-for i in range(len(Postcode_Details)):
-    Suburb = Postcode_Details.at[i,"Suburb"]
-    Postcode = Postcode_Details.at[i,"Postcode"]
-    Suburb_Search = str(Suburb) + ", NSW " + str(Postcode)
-    #print(Suburb_Search)
+
+def Main():
+    for i in range(len(Postcode_Details)):
+        Suburb = Postcode_Details.at[i,"Suburb"]
+        Postcode = Postcode_Details.at[i,"Postcode"]
+        Suburb_Search = str(Suburb) + ", NSW " + str(Postcode)
+        print(Suburb_Search)
+        Yellow_Page_Search(Suburb_Search)
     
-def Yellow_Page_Search():
+def Yellow_Page_Search(Suburb_Search):
     browser = webdriver.Chrome()
     browser.get("https://www.yellowpages.com.au/")
     Trade_Enter = browser.find_element_by_id('clue')
@@ -25,13 +26,11 @@ def Yellow_Page_Search():
     time.sleep(2)
     browser.find_element_by_id('where').clear()
     Suburb_Enter = browser.find_element_by_id('where')
-    Suburb_Enter.send_keys("Blacktown, NSW 2148")
+    Suburb_Enter.send_keys(Suburb_Search)
     Search_Button = browser.find_element_by_xpath('/html/body/div[1]/div/div[3]/div/div/form/div/div[3]/div/button/span')
     Search_Button.click()
     CurrentURL = browser.current_url
-    print(CurrentURL)
-Yellow_Page_Search()
+    browser.quit()
 
-#login_buttom = browser.find_element_by_id('loginbutton')
-#login_buttom.click()
-
+#Yellow_Page_Search("Blacktown, NSW 2148")
+Main()
